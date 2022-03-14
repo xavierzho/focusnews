3f62d3446abe5232f0dde75ec46466686a3e79f7
xjones <jonescyna@gmail.com>
2022-03-14 14:25:05 +0800

    add daemon flags

--- api/api.go
+++ api/api.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"api/config"
+	"api/handlers"
 	"context"
+	"flag"
 	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
 
 	"github.com/ilyakaznacheev/cleanenv"
 	"github.com/labstack/echo/v4"
-
-	"api/config"
-	"api/handlers"
-
+	"github.com/sevlyar/go-daemon"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-	e       = echo.New()
-	newsDB  *mongo.Database
-	newsCol *mongo.Collection
-	cfg     config.Properties
+	e         = echo.New()
+	newsDB    *mongo.Database
+	newsCol   *mongo.Collection
+	cfg       config.Properties
+	ErrSignal = fmt.Errorf("signal valid")
+	s         = &http.Server{}
 )
 
 func init() {
@@ -35,8 +42,8 @@ func init() {
 	newsDB = c.Database(cfg.NewsDatabase)
 	newsCol = newsDB.Collection(cfg.NewsCollection)
 }
-func main() {
 
+func server() {
 	nh := &handlers.NewsHandler{Col: newsCol}
 	// news routers
 	e.GET("/news", nh.GetNewsS)
@@ -48,4 +55,72 @@ func main() {
 
 	// server start
 	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)))
+}
+func main() {
+	os.Args[0], _ = filepath.Abs(os.Args[0])
+	signals := flag.String("s", "", "send signal to daemon")
+	daemon.AddCommand(daemon.StringFlag(signals, "stop"), syscall.SIGTERM, Shutdown)
+	daemon.AddCommand(daemon.StringFlag(signals, "reload"), syscall.SIGHUP, Reload)
+
+	flag.Parse()
+	dmn := &daemon.Context{
+		PidFileName: "./log/pinksale.pid",
+		PidFilePerm: 0644,
+		LogFileName: "./log/pinksale.log",
+		LogFilePerm: 0640,
+		WorkDir:     "/",
+		Umask:       027,
+	}
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := dmn.Search()
+		if err != nil {
+			fmt.Println("Unable send signal to the daemon:", err)
+		}
+		_ = daemon.SendCommands(d)
+		return
+	}
+	child, err := dmn.Reborn()
+	if err != nil {
+		fmt.Println("reborn:", err)
+	}
+	if child != nil {
+		return
+	}
+	defer dmn.Release()
+
+	// business logic
+	go server()
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+func Shutdown(sig os.Signal) error {
+	if sig != syscall.SIGTERM {
+		return ErrSignal
+	}
+	//common.LOG.Info("system exit later...")
+	os.Exit(0)
+	return nil
+}
+func Reload(sig os.Signal) error {
+	if sig != syscall.SIGHUP {
+		return ErrSignal
+	}
+	s.RegisterOnShutdown(func() {
+		path := os.Args[0]
+		cmd := exec.Command(path, os.Args[1:]...)
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		cmd.Stderr = os.Stderr
+		cmd.Env = os.Environ()
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("reload failed")
+
+		}
+		fmt.Println("system reload success ...")
+	})
+	return s.Shutdown(context.Background())
 }
--- api/go.mod
+++ api/go.mod
@@ -5,9 +5,11 @@ go 1.15
 require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.2.5
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/labstack/echo/v4 v4.2.1
 	github.com/labstack/gommon v0.3.0
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/sevlyar/go-daemon v0.1.5 // indirect
 	go.mongodb.org/mongo-driver v1.8.2
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
